a6e131c85db996880da46fabf7803f76af3a20a0
Marceline Belardo <marcelinebelardo@gmail.com>
2025-06-27 17:18:08 +0300

    fixing up the string stuff with parser, added types

--- src/protocol.zig
+++ src/protocol.zig
@@ -1,27 +1,93 @@
 const std = @import("std");
+const fmt = std.fmt;
 
-// simple protocol
-// RESP-like, without arrays/bulks yet
-// PING\r\n\r\n -> PONG\r\n\r\n
-// SET\r\nKEY\r\nVALUE\r\n\r\n -> OK\r\n
-// GET\r\nKEY\r\n\r\n -> OK\r\n
-// DEL\r\nKEY\r\n\r\n -> OK\r\n
+// Simple string "+PING\r\n"
+// Error "-ERR message\r\n"
+// Int ":128\r\n"
+// Bulk "$<length>\r\n<data>\r\n" with empty "$0\r\n\r\n"
+// Array "*<num of elems>\r\n<elem 1>...<elem n>
 
-fn parse(buf: []const u8) !void {
-    var tokens = readToTokenSequence(buf);
+const MsgType = enum {
+    simple,
+    err,
+    int,
+    bulk,
+    arr,
+};
 
+const Msg = union(MsgType) {
+    simple: []const u8,
+    err: []const u8,
+    int: i64,
+    bulk: []const u8,
+    arr: []Msg,
+};
+
+const ProtocolError = error{
+    SyntaxError,
+};
+
+fn parse(buf: []const u8) !Msg {
+    for (buf) |c| {
+        switch (c) {
+            '+' => {
+                step(buf);
+                return Msg{ .simple = try readLine(buf) };
+            },
+            '-' => {
+                step(buf);
+                return Msg{ .err = try readLine(buf) };
+            },
+            ':' => {
+                step(buf);
+                return Msg{ .int = fmt.parseInt(i64, try readLine(buf)) };
+            },
+            '$' => {
+                step(buf);
+                const len = fmt.parseInt(usize, try readLine(buf));
+                const start_pos = std.mem.indexOf(u8, buf, "\r\n") + 2;
+                return Msg{ .bulk = buf[start_pos..len] };
+            },
+            '*' => {
+                step(buf);
+                const elems = fmt.parseInt(usize, try readLine(buf));
+                const start_pos = std.mem.indexOf(u8, buf, "\r\n") + 2;
+                var arr: [elems]Msg = undefined;
+
+                for (elems) |e| {
+                    arr[e] = parse(buf[start_pos..]);
+                }
+
+                return Msg{ .arr = arr };
+            },
+            else => return ProtocolError.SyntaxError,
+        }
+    }
 }
 
-fn readToTokenSequence(buf: []const u8) std.mem.TokenIterator(u8, .sequence) {
+fn readLine(buf: []const u8) ![]const u8 {
     const delim: []const u8 = "\r\n";
-    return std.mem.tokenizeSequence(u8, buf, delim);
+    const delim_pos = try std.mem.indexOf(u8, buf, delim);
+
+    if (delim_pos) |pos| {
+        return buf[0..pos];
+    } else {
+        return ProtocolError.SyntaxError;
+    }
+}
+
+fn step(buf: []const u8) void {
+    buf += 1;
 }
 
 test "gets a line" {
     const ping: []const u8 = "PING\r\n";
-    var seq = readToTokenSequence(ping);
-    try expect(std.mem.eql(u8, seq.next().?, "PING"));
-    try expect(seq.next() == null);
+    try expect(std.mem.eql(u8, readLine(ping), "PING"));
+}
+
+test "simple string" {
+    const message: []const u8 = "+PING\r\n";
+    try expect(std.mem.eql(Msg, parse(message), Msg{ .simple = "PING" }));
 }
 
 const expect = std.testing.expect;
