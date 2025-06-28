01c109e2e03f644a4092ab3a6ecb10f7e9ef2cf2
Marceline Belardo <marcelinebelardo@gmail.com>
2025-06-28 23:34:29 +0300

    parsing works, except for bulk, add array type again

--- src/protocol.zig
+++ src/protocol.zig
@@ -26,42 +26,32 @@ const ProtocolError = error{
 };
 
 fn parse(buf: []const u8, elems: *std.ArrayList(Msg)) !void {
-    for (buf) |c| {
-        switch (c) {
-            '+' => {
-                step(buf);
-                try elems.append(Msg{ .simple = try readLine(buf) });
-            },
-            '-' => {
-                step(buf);
-                try elems.append(Msg{ .err = try readLine(buf) });
-            },
-            ':' => {
-                step(buf);
-                const n = try fmt.parseInt(i64, try readLine(buf), 10);
-                try elems.append(Msg{ .int = n });
-            },
-            '$' => {
-                step(buf);
-                const len = try fmt.parseInt(usize, try readLine(buf), 10);
-                const crlf_idx = std.mem.indexOf(u8, buf, "\r\n")
-                    orelse return ProtocolError.SyntaxError;
-                const start_pos = crlf_idx + 2;
-                try elems.append(Msg{ .bulk = buf[start_pos..len] });
-            },
-            '*' => {
-                step(buf);
-                const elem_num = try fmt.parseInt(usize, try readLine(buf), 10);
-                const crlf_idx = std.mem.indexOf(u8, buf, "\r\n")
-                    orelse return ProtocolError.SyntaxError;
-                const start_pos = crlf_idx + 2;
+    switch (buf[0]) {
+        '+' => {
+            try elems.append(Msg{ .simple = try readLine(buf[1..]) });
+        },
+        '-' => {
+            try elems.append(Msg{ .err = try readLine(buf[1..]) });
+        },
+        ':' => {
+            const n = try fmt.parseInt(i64, try readLine(buf[1..]), 10);
+            try elems.append(Msg{ .int = n });
+        },
+        '$' => {
+            const len = try fmt.parseInt(usize, try readLine(buf[1..]), 10);
+            const start_pos = try findDataStart(buf);
+            try elems.append(Msg{ .bulk = buf[start_pos..len] });
+        },
+        '*' => {
+            const elem_num = try fmt.parseInt(usize, try readLine(buf[1..]), 10);
+            const start_pos = try findDataStart(buf);
 
-                for (0..elem_num) |_| {
-                    try parse(buf[start_pos..], elems);
-                }
-            },
-            else => return ProtocolError.SyntaxError,
-        }
+            for (0..elem_num) |_| {
+                const line = try readLine(buf[start_pos..]);
+                try parse(line, elems);
+            }
+        },
+        else => return ProtocolError.SyntaxError,
     }
 }
 
@@ -76,16 +66,76 @@ fn readLine(buf: []const u8) ![]const u8 {
     }
 }
 
-fn step(buf: []const u8) void {
-    buf += 1;
+fn findDataStart(buf: []const u8) !usize {
+    const delim: []const u8 = "\r\n";
+    const delim_pos = std.mem.indexOf(u8, buf, delim);
+
+    if (delim_pos) |pos| {
+        return pos + 2;
+    } else {
+        return ProtocolError.SyntaxError;
+    }
 }
 
 test "simple string" {
     const ping: []const u8 = "+PING\r\n";
     var elems = std.ArrayList(Msg).init(test_allocator);
-    const tokens = try parse(ping, &elems);
-    try expect(tokens[0], Msg{ .simple = "PING" });
+    defer elems.deinit();
+    try parse(ping, &elems);
+
+    switch (elems.items[0]) {
+        .simple => |str| try expect(std.mem.eql(u8, str, "PING")),
+        else => unreachable,
+    }
 }
+
+test "error message" {
+    const err: []const u8 = "-ERR message\r\n";
+    var elems = std.ArrayList(Msg).init(test_allocator);
+    defer elems.deinit();
+    try parse(err, &elems);
+
+    switch (elems.items[0]) {
+        .err => |str| try expect(std.mem.eql(u8, str, "ERR message")),
+        else => unreachable,
+    }
+}
+
+test "integer" {
+    const n: []const u8 = ":128\r\n";
+    var elems = std.ArrayList(Msg).init(test_allocator);
+    defer elems.deinit();
+    try parse(n, &elems);
+
+    switch (elems.items[0]) {
+        .int => |num| try expect(num == 128),
+        else => unreachable,
+    }
+}
+
+test "bulk string" {
+    const bulk: []const u8 = "$20\r\nhello!\r\nhow are you?\r\n";
+    var elems = std.ArrayList(Msg).init(test_allocator);
+    defer elems.deinit();
+    try parse(bulk, &elems);
+
+    switch (elems.items[0]) {
+        .bulk => |str| {
+            std.debug.print("[{s}]\nLEN: {}\n", .{str, str.len});
+            try expect(std.mem.eql(u8, str, "hello!\r\nhow are you?"));
+        },
+        else => unreachable,
+    }
+}
+
+// make an array type again
+// test "array message" {
+//     const arr: []const u8 = "*2\r\n+OK\r\n:10\r\n";
+//     var elems = std.ArrayList(Msg).init(test_allocator);
+//     defer elems.deinit();
+//     try parse(arr, &elems);
+//
+// }
 
 const expect = std.testing.expect;
 const test_allocator = std.testing.allocator;
